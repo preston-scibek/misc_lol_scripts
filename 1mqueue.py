eb0c986a5d8154ec742d1f75dfaf5fd41cee52ee
Preston <preston-scibek@users.noreply.github.com>
2021-06-05 17:02:34 -0400

    Fixed the function
    - changed the script to read from the lockfile to avoid the user having to deal with rift-explore and editing the file
    - moved the code into a function cuz i can
    - cleaned up some variables and other aspects

--- 1mqueue.py
+++ 1mqueue.py
@@ -1,17 +1,49 @@
 __author__ = "Generalbood"
-
+""" This file restarts your queue after one minute in 
+an attempt to minimze offroling/autofilling"""
 import json
 import os
 import time
+import base64
 
-# Get Token From developer.riotgames.com
-token = "notLeakingMyToken"
-# IDK if host changes with each launch, might need rift explorer to find it. https://github.com/Pupix/rift-explorer
-host = "https://127.0.0.1:44563"
-while(json.loads(os.popen("curl -X GET --header 'Accept: application/json' --header 'Authorization: Basic {}' '{}/lol-champ-select/v1/session' -k".format(token, host)).read())['httpStatus'] == 404):
-        # launch
-        os.system("curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' --header 'Authorization: Basic {}' '{}/lol-lobby/v2/lobby/matchmaking/search' -k".format(token, host))
-        time.sleep(58)
-        # kill
-        os.system("curl -X DELETE --header 'Accept: application/json' --header 'Authorization: Basic {}' '{}/lol-lobby/v2/lobby/matchmaking/search' -k".format(token, host))
+def one_minute_queue():
+    """this function restarts your queue after one minute has elaspsed"""
+    # read from lockfile
+    # replace this with your league install
+    lockfile_location =  "/mnt/c/Riot Games/League of Legends/lockfile"
+    with open(lockfile_location, "r") as lockfile:
+        lockfile_array = lockfile.read().split(":")
+        port = lockfile_array[2]
+        token = lockfile_array[3]
 
+    host = "127.0.0.1:{}".format(port)
+    # encode the auth token
+    token_bytes = "riot:{}".format(token).encode('ascii')
+    token = base64.b64encode(token_bytes).decode('utf-8')
+
+    # Generate the curl strings
+    champ_select_check = "curl -X GET --header 'Accept: application/json' " +\
+        "--header 'Authorization: Basic {}' 'https://{}/lol-champ-select/v1/session' -k"
+    champ_select_check = champ_select_check.format(token, host)
+
+    start_queue_curl = "curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' " + \
+        "--header 'Authorization: Basic {}' " + \
+        "'https://{}/lol-lobby/v2/lobby/matchmaking/search' -k"
+    start_queue_curl = start_queue_curl.format(token, host)
+
+    stop_queue_curl = "curl -X DELETE --header 'Content-Type: application/json' --header 'Accept: application/json' " + \
+        "--header 'Authorization: Basic {}' " + \
+        "'https://{}/lol-lobby/v2/lobby/matchmaking/search' -k"
+    stop_queue_curl = stop_queue_curl.format(token, host)
+
+    # execute the core loop
+    while(json.loads(os.popen(champ_select_check).read())['httpStatus'] == 404):
+            # start queue
+            os.system(start_queue_curl)
+            time.sleep(58)
+            # stop queue
+            os.system(stop_queue_curl)
+
+
+if __name__ == "__main__":
+    one_minute_queue()
