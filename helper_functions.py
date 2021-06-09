755a4e16502a6f6ce218a35cfa906de165dda462
Preston Scibek <psscibek@gmail.com>
2021-06-09 11:41:20 -0400

    switched to requests cuz lmao why was i using curl

--- helper_functions.py
+++ helper_functions.py
@@ -6,6 +6,12 @@ import os
 import time
 import base64
 
+import requests
+
+# fuck the ssl warning
+import warnings
+warnings.filterwarnings("ignore")
+
 def get_credentials():
     """Retreive the current client credentials from the lock file"""
     # read from lockfile
@@ -23,14 +29,37 @@ def get_credentials():
     return host, token
 
 
-def call_endpoint(endpoint, method, *args):
+def call_endpoint(endpoint, method, *args, **kwargs):
     """Call the specified LCU endpoint via the given method , substituting in *args as needed"""
     host, token = get_credentials()
     formatted_endpoint = endpoint.format(*args)
-    curl_command = "curl -X {} --header 'Accept: application/json' " +\
-        "--header 'Authorization: Basic {}' 'https://{}{}' -k"
-    curl_command = curl_command.format(method, token, host, formatted_endpoint)
-    res_str = os.popen(curl_command).read()
-    if res_str:
-        return json.loads(res_str)
-    return {"response": res_str}
+   
+    headers = {
+        'Authorization': 'Basic {}'.format(token)
+    }
+
+    data = kwargs.get('data', None)
+    full_url = "https://{}{}".format(host, formatted_endpoint)
+
+    if data:
+        if method == 'POST':
+            res = requests.post(full_url, headers=headers, data=json.dumps(data), verify=False)
+        elif method == 'PUT':
+            res = requests.put(full_url, headers=headers, data=json.dumps(data), verify=False)
+        elif method == 'PATCH':
+            res = requests.patch(full_url, headers=headers, data=json.dumps(data), verify=False)
+    else:
+        if method == 'GET':
+            res = requests.get(full_url, headers=headers, verify=False)
+        elif method == 'DELETE':
+            res = requests.delete(full_url, headers=headers, verify=False)
+        elif method == 'POST':
+            res = requests.post(full_url, headers=headers, verify=False)
+        elif method == 'PUT':
+            res = requests.put(full_url, headers=headers, verify=False)
+        elif method == 'PATCH':
+            res = requests.patch(full_url, headers=headers, verify=False)
+    try:
+        return res.json()
+    except ValueError:
+        return res
